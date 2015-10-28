050c7bc65668787b12da667a174132f5f7ba5078
Gian Zonetti <gianzonetti@gmail.com>
2015-10-28 17:04:16 -0200

    Solution Desafio de organizar os grupos

---
+++ DesafioOrganizarGrupos/DesafioOrganizarGrupos/Classes/AlunoOperations.cs
@@ -0,0 +1,99 @@
+﻿using DesafioOrganizarGrupos.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAlunos.Classes
+{
+    public static class AlunoOperation
+    {
+
+        /// <summary>
+        /// Cria um objeeto de aluno.
+        /// </summary>
+        /// <param name="aluno"></param>
+        /// <param name="i"></param>
+        public static void AddAluno(Aluno aluno, int i)
+        {
+            do
+            {
+                Console.WriteLine("Informe o nome do {0}° aluno: ", i + 1);
+                aluno.Nome = Console.ReadLine();
+            } while (aluno.Nome == string.Empty);
+        }
+
+
+        /// <summary>
+        /// Organiza os alunos nos grupos
+        /// </summary>
+        /// <param name="turma"></param>
+        /// <param name="grupos"></param>
+        /// <param name="numeroDeGrupos"></param>
+
+        public static void OrganizaGrupos(List<Aluno> turma, List<Aluno>[] grupos, int numeroDeGrupos)
+        {
+            Random rand = new Random();
+            int idGrupo = 0;
+
+            Stack<int> pilha = new Stack<int>();
+            foreach (var item in turma)
+            {
+                do
+                {
+                    idGrupo = rand.Next(numeroDeGrupos);
+                } while (!NaoRepetirRandom(idGrupo, pilha, numeroDeGrupos));
+
+                grupos[idGrupo].Add(item);
+
+
+            }
+        }
+
+
+        /// <summary>
+        /// Vai adicionar os alunos dentro da lista Turma.
+        /// </summary>
+        /// <param name="numeroDeAluno"></param>
+        /// <param name="turma"></param>
+        public static void AddAlunoTurma(int numeroDeAluno, List<Aluno> turma)
+        {
+            for (int i = 0; i < numeroDeAluno; i++)
+            {
+                Aluno aluno = new Aluno();
+                aluno.Nome = string.Empty;
+
+                Console.Clear();
+
+                AlunoOperation.AddAluno(aluno, i);
+
+                turma.Add(aluno);
+            }
+        }
+
+
+        /// <summary>
+        /// Validação para o random não repetir numero do grupo.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="pilha"></param>
+        /// <param name="numeroDeGrupos"></param>
+        /// <returns></returns>
+        public static bool NaoRepetirRandom(int i, Stack<int> pilha, int numeroDeGrupos)
+        {
+
+            if (pilha.Count == numeroDeGrupos)
+                pilha.Clear();
+
+            if (pilha.Contains(i))
+                return false;
+            else
+                pilha.Push(i);
+            return true;
+        }
+
+
+    }
+}
+
---
+++ DesafioOrganizarGrupos/DesafioOrganizarGrupos/Program.cs
@@ -0,0 +1,76 @@
+﻿using DesafioAlunos.Classes;
+using DesafioOrganizarGrupos.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAlunos
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int numeroDeAluno = 0;
+            List<Aluno> turma = new List<Aluno>();
+
+
+            // Informando quantos alunos vai ter na turma
+            do
+            {
+                Console.WriteLine("Informe a quantidade de alunos na turma: ");
+                numeroDeAluno = int.Parse(Console.ReadLine());
+            } while (numeroDeAluno <= 0);
+
+            // Adicionando os alunos
+            AlunoOperation.AddAlunoTurma(numeroDeAluno, turma);
+
+            // Vai informar o numero de grupos que quer.
+            int numeroDeGrupos = 0;
+            Console.Clear();
+
+            do
+            {
+                Console.WriteLine("Informe o numero de grupos que deseja formar: ");
+                numeroDeGrupos = int.Parse(Console.ReadLine());
+            } while (numeroDeGrupos <= 0);
+
+            List<Aluno>[] grupos = new List<Aluno>[numeroDeGrupos];
+
+            for (int i = 0; i < numeroDeGrupos; i++)
+            {
+                grupos[i] = new List<Aluno>();
+            }
+            // Separação dos alunos
+
+            AlunoOperation.OrganizaGrupos(turma, grupos, numeroDeGrupos);
+
+            // Apresentando os grupos
+
+            ApresentarGrupos(numeroDeGrupos, grupos);
+
+            Console.ReadKey();
+        }
+
+
+
+        /// <summary>
+        ///  Apresenta os alunos pertencentes a cada grupo
+        /// </summary>
+        /// <param name="numeroDeGrupos"></param>
+        /// <param name="grupos"></param>
+        private static void ApresentarGrupos(int numeroDeGrupos, List<Aluno>[] grupos)
+        {
+            for (int i = 0; i < numeroDeGrupos; i++)
+            {
+                Console.WriteLine("\n -GRUPO {0} \n", i);
+                foreach (var aluno in grupos[i])
+                {
+                    Console.WriteLine(aluno.Nome);
+                }
+            }
+        }
+    }
+}
