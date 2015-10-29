93099511c79cc19c67c4030e05c0bc1db35e3cef
Gian Zonetti <gianzonetti@gmail.com>
2015-10-29 13:52:06 -0200

    Tirando bibliotecas desnecessárias

--- DesafioOrganizarGrupos/DesafioOrganizarGrupos/Classes/Aluno.cs
+++ DesafioOrganizarGrupos/DesafioOrganizarGrupos/Classes/Aluno.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesafioOrganizarGrupos.Classes
+﻿namespace DesafioOrganizarGrupos.Classes
 {
     public class Aluno
     {
--- DesafioOrganizarGrupos/DesafioOrganizarGrupos/Classes/AlunoOperations.cs
+++ DesafioOrganizarGrupos/DesafioOrganizarGrupos/Classes/AlunoOperations.cs
@@ -1,9 +1,6 @@
 ﻿using DesafioOrganizarGrupos.Classes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DesafioAlunos.Classes
 {
